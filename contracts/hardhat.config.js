54618604b44be1e2bcfe41f037a36ff7ced83a12
Jannik Luhn <jannik@brainbot.com>
2021-07-14 22:06:23 +0200

    Add deploy script

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
@@ -10,4 +11,27 @@ module.exports = {
       },
     },
   },
+
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/cb47771bf3324acc895994de6752654b",
+      accounts: [
+        process.env.DEPLOY_KEY_GOERLI,
+      ],
+    },
+  },
 };
+
+task("deploy", "Deploys the contract")
+  .setAction(async () => {
+    const factory = await ethers.getContractFactory("MEVsky");
+    const contract = await factory.deploy();
+    console.log("deploy tx:", contract.deployTransaction.hash);
+    const receipt = await contract.deployTransaction.wait();
+    if (receipt.status == 1) {
+      console.log("contract deployed at", receipt.contractAddress);
+    } else {
+      console.log("deploy transaction failed");
+      console.log(receipt);
+    }
+  });
