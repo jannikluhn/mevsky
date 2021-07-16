92ff95fcf861e774046ea9f272c4c582068eed05
Jannik Luhn <jannik@brainbot.com>
2021-07-17 00:52:46 +0200

    Page stuff

--- interface/src/config.js
+++ interface/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   defaultRPCProviderURL: 'https://rpc.xdaichain.com/',
-  contractAddress: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+  contractAddress: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
   chainId: 31337,
 };
