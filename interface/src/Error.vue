9315b804263de93c4e31498410e27188e617753a
Jannik Luhn <jannik@brainbot.com>
2021-07-29 00:31:18 +0200

    Add error component
    It's not used right now

---
+++ interface/src/Error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="error">
+    <!-- <h1>Error</h1>
+    <p>{{ error.message }}</p> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  props: [
+    'error',
+  ],
+};
+</script>
+
+<style scoped>
+.error {
+  /* position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 500px;
+  height: 300px;
+  margin: auto;
+  background-color: red; */
+}
+</style>
