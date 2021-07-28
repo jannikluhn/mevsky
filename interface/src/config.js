ade4af76e2da4bfa3b7577d213175aacda06af44
Jannik Luhn <jannik@brainbot.com>
2021-07-29 00:30:40 +0200

    Use goerli

--- interface/src/config.js
+++ interface/src/config.js
@@ -1,5 +1,11 @@
+// module.exports = {
+//   backupRPCProviderURL: 'https://rpc.xdaichain.com/',
+//   contractAddress: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+//   chainId: 31337,
+// };
+
 module.exports = {
-  defaultRPCProviderURL: 'https://rpc.xdaichain.com/',
-  contractAddress: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-  chainId: 31337,
+  backupRPCProviderURL: 'https://goerli.infura.io/v3/cb47771bf3324acc895994de6752654b',
+  contractAddress: '0x284f6A78BB277716B6397fEd2Efe9D2FEfb8731e',
+  chainId: 5,
 };
